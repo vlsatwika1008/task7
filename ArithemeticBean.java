02569290dc39bd65a9a053ce4ae06659981bfdd2
vlsatwika1008 <vlsatwika1008@gmail.com>
2026-05-19 04:53:57 +0530

    Update ArithemeticBean.java

--- ArithemeticBean.java
+++ ArithemeticBean.java
@@ -1,46 +1,31 @@
 //  javac -d . ArithmeticBean.java
-
-package beans;
-
-import java.io.Serializable;
-
-public class ArithmeticBean implements Serializable {
-    private double result;
-
-    public ArithmeticBean() {}
-
-    // Method to perform calculation based on operation
-    public void calculate(String operation, double num1, double num2) {
-        if (operation == null) return;
-        
-        switch (operation.toLowerCase()) {
-            case "add":
-                result = num1 + num2;
-                break;
-            case "subtract":
-                result = num1 - num2;
-                break;
-            case "multiply":
-                result = num1 * num2;
-                break;
-            case "divide":
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    result = Double.NaN; // Handle division by zero
-                }
-                break;
-            default:
-                result = 0;
-                break;
-        }
-    }
-
-    public double getResult() {
-        return result;
-    }
-}
-
+package beans; 
+public class ArithmeticBean { 
+private double num1; 
+private double num2; 
+private double result; 
+private String operation; 
+// Getters and Setters 
+public double getNum1() { return num1; } 
+public void setNum1(double num1) { this.num1 = num1; } 
+public double getNum2() { return num2; } 
+public void setNum2(double num2) { this.num2 = num2; } 
+public String getOperation() { return operation; } 
+public void setOperation(String operation) { this.operation = operation; } 
+public double getResult() { return result; } 
+public void calculate() { 
+switch (operation) { 
+case "add": result = num1 + num2; break; 
+case "subtract": result = num1 - num2; break; 
+case "multiply": result = num1 * num2; break; 
+case "divide": 
+if (num2 != 0) result = num1 / num2; 
+else result = Double.NaN; // Handle divide by zero 
+break; 
+default: result = 0; 
+} 
+} 
+} 
 
 // folder in xampp/tomcat/webapps
 // MyArithmeticApp/
